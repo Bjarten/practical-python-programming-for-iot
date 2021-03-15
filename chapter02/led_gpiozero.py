8db5d854be071c360f3b358445e4aa6a0dece926
Bjarten <BjarteSunde@outlook.com>
2021-03-15 08:18:39 +0100

    add: scripts from support repo

---
+++ chapter02/led_gpiozero.py
@@ -0,0 +1,22 @@
+"""
+File: chapter02/led_gpiozero.py
+
+Blinking an LED using GPIOZero.
+
+Dependencies:
+  pip3 install gpiozero pigpio
+
+Built and tested with Python 3.7 on Raspberry Pi 4 Model B
+"""
+from gpiozero import Device, LED                                                  # (1)
+from gpiozero.pins.pigpio import PiGPIOFactory                                    # (2)
+from time import sleep
+
+Device.pin_factory = PiGPIOFactory() #Set gpiozero to use pigpio by default.      # (3)
+
+GPIO_PIN = 21
+led = LED(GPIO_PIN)                                                               # (4)
+# Blink for 10 seconds
+print("Blinking for 10 seconds...")
+led.blink(background=False, n=10)                                                       # (5)
+print("Done")
